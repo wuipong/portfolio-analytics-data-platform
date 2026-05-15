55c1fd38253a40cc1f5ec2aaaaead7b62f4d9ed6
WP <57185147+wuipong@users.noreply.github.com>
2026-05-15 18:45:11 +0100

    Create int_daily_positions_enriched.sql for enriched data

---
+++ dbt/models/intermediate/int_daily_positions_enriched.sql
@@ -0,0 +1,14 @@
+WITH holdings AS (SELECT * FROM {{ ref('stg_holdings') }}),
+prices AS (SELECT * FROM {{ ref('stg_market_prices') }}),
+fx AS (SELECT * FROM {{ ref('stg_fx_rates') }} WHERE quote_currency = 'USD')
+
+SELECT
+    h.*,
+    p.market_price AS latest_market_price,
+    p.yield_to_maturity,
+    COALESCE(f.fx_rate, 1.0) AS usd_fx_rate,
+    -- 計算調整後的 USD 市值
+    (h.market_value * COALESCE(f.fx_rate, 1.0)) AS market_value_usd
+FROM holdings h
+LEFT JOIN prices p ON h.isin = p.isin AND h.valuation_date = p.valuation_date
+LEFT JOIN fx f ON h.currency = f.base_currency AND h.valuation_date = f.valuation_date
