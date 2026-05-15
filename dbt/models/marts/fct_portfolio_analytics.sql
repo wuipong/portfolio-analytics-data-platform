cca46c6a37c120cc681e72dd5f0c8cb2255160f0
WP <57185147+wuipong@users.noreply.github.com>
2026-05-15 18:49:30 +0100

    Refactor fct_portfolio_analytics.sql for portfolio analysis

--- dbt/models/marts/fct_portfolio_analytics.sql
+++ dbt/models/marts/fct_portfolio_analytics.sql
@@ -1,33 +1,22 @@
-{{ config(materialized='table') }}
-
-WITH silver_data AS (
-    SELECT * FROM {{ source('silver', 'int_normalized_holdings') }}
-    WHERE Quality_Flag = 'CLEAN'
-),
-
-ratings AS (
-    SELECT * FROM {{ ref('stg_credit_ratings') }}
-),
-
-total_sum AS (
-    SELECT SUM(MarketValue_USD) as total_mv FROM silver_data
+WITH enriched AS (SELECT * FROM {{ ref('int_daily_positions_enriched') }}),
+portfolio_totals AS (
+    SELECT 
+        valuation_date, 
+        portfolio_id, 
+        SUM(market_value_usd) AS total_portfolio_mv 
+    FROM enriched 
+    GROUP BY 1, 2
 )
 
 SELECT
-    s.AsOfDate,
-    s.ISIN,
-    s.AssetType,
-    s.MarketValue_USD,
-    -- 計算資產權重 (Key Performance Area: Quantitative analysis)
-    (s.MarketValue_USD / t.total_mv) as Portfolio_Weight,
-    r.Rating_Provider,
-    r.Credit_Rating,
-    -- 映射評級分數以進行數值化風險分析
-    CASE 
-        WHEN r.Credit_Rating = 'AAA' THEN 1
-        WHEN r.Credit_Rating = 'AA' THEN 2
-        ELSE 10 
-    END as Rating_Score
-FROM silver_data s
-CROSS JOIN total_sum t
-LEFT JOIN ratings r ON s.ISIN = r.ISIN
+    e.valuation_date,
+    e.portfolio_id,
+    e.isin,
+    e.market_value_usd,
+    -- weighting calculation (Automated portfolio analysis)
+    e.market_value_usd / t.total_portfolio_mv AS portfolio_weight,
+    -- Calculate Duration Contribution
+    e.duration * (e.market_value_usd / t.total_portfolio_mv) AS duration_contribution,
+    e.yield_to_maturity
+FROM enriched e
+JOIN portfolio_totals t ON e.portfolio_id = t.portfolio_id AND e.valuation_date = t.valuation_date
